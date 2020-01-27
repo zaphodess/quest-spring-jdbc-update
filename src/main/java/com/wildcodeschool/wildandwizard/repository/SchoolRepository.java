749e58b05ca3d8ef5ea87fb0522cf652e8e110a8
zaphodess <monika.messerer@telekom.de>
2020-01-27 12:11:59 +0100

    Easy this time

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -7,48 +7,64 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.entity.Wizard;
 import com.wildcodeschool.wildandwizard.util.JdbcUtils;
 
 public class SchoolRepository {
 
-    private final static String DB_URL = "jdbc:mysql://localhost:3306/spring_jdbc_quest?serverTimezone=GMT";
-    private final static String DB_USER = "h4rryp0tt3r";
-    private final static String DB_PASSWORD = "Horcrux4life!";
+	private final static String DB_URL = "jdbc:mysql://localhost:3306/spring_jdbc_quest?serverTimezone=GMT";
+	private final static String DB_USER = "h4rryp0tt3r";
+	private final static String DB_PASSWORD = "Horcrux4life!";
 
-    public School update(Long id, String name, Long capacity, String country) {
+	public School update(Long id, String name, Long capacity, String country) {
 
-        // TODO : update a school from the database
-        return null;
-    }
+		Connection connection = null;
+		PreparedStatement statement = null;
+		try {
+			connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			statement = connection.prepareStatement("UPDATE school SET name=?, capacity=?, country=? WHERE id=?");
+			statement.setString(1, name);
+			statement.setLong(2, capacity);
+			statement.setString(3, country);
+			statement.setLong(4, id);
 
-    public School findById(Long id) {
+			if (statement.executeUpdate() != 1) {
+				throw new SQLException("failed to update data");
+			}
+			return new School(id, name, capacity, country);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtils.closeStatement(statement);
+			JdbcUtils.closeConnection(connection);
+		}
+		return null;
+	}
 
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        try {
-            connection = DriverManager.getConnection(
-                    DB_URL, DB_USER, DB_PASSWORD
-            );
-            statement = connection.prepareStatement(
-                    "SELECT * FROM school WHERE id = ?;"
-            );
-            statement.setLong(1, id);
-            resultSet = statement.executeQuery();
+	public School findById(Long id) {
 
-            if (resultSet.next()) {
-                String name = resultSet.getString("name");
-                Long capacity = resultSet.getLong("capacity");
-                String country = resultSet.getString("country");
-                return new School(id, name, capacity, country);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            JdbcUtils.closeResultSet(resultSet);
-            JdbcUtils.closeStatement(statement);
-            JdbcUtils.closeConnection(connection);
-        }
-        return null;
-    }
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			statement = connection.prepareStatement("SELECT * FROM school WHERE id = ?;");
+			statement.setLong(1, id);
+			resultSet = statement.executeQuery();
+
+			if (resultSet.next()) {
+				String name = resultSet.getString("name");
+				Long capacity = resultSet.getLong("capacity");
+				String country = resultSet.getString("country");
+				return new School(id, name, capacity, country);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtils.closeResultSet(resultSet);
+			JdbcUtils.closeStatement(statement);
+			JdbcUtils.closeConnection(connection);
+		}
+		return null;
+	}
 }
